7e77c2fd65ca4f188d4a5e879d9605b3914088d2
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-17 03:23:53 -0300

    fix: Realizados ajustes na query da rota /users/me, uma vez que, quando um usuário sem links criados logava/tentava criar um link, crashava a aplicação.

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,20 +9,23 @@ async function getUserHistoric(req, res) {
       SELECT 
         users.id,
         users.name,
-        SUM(links."visitCount") AS "visitCount",
-        json_agg(json_build_object('id', links.id, 'shortUrl', links."shortUrl", 'url', links.url, 'visitCount', links."visitCount")) AS "shortenedUrls"
+        COALESCE(SUM(links."visitCount"), 0) AS "visitCount",
+        CASE
+          WHEN "visitCount" IS NULL THEN json_build_array()
+          ELSE json_agg(json_build_object('id', links.id, 'shortUrl', links."shortUrl", 'url', links.url, 'visitCount', links."visitCount"))
+        END AS "shortenedUrls"
       FROM users
-      JOIN links
+      LEFT JOIN links
         ON users.id = links."userId"
       WHERE users.id = $1
-      GROUP BY users.id
+      GROUP BY users.id, links."visitCount"
     `,
 			[userId]
 		);
-
+		historic.rows[0].visitCount = Number(historic.rows[0].visitCount);
 		return res.status(200).send(historic.rows[0]);
 	} catch (error) {
-		return res.status(500).sendo(error.message);
+		return res.status(500).send(error.message);
 	}
 }
 
@@ -43,7 +46,9 @@ async function usersRanking(req, res) {
     `);
 
 		return res.status(200).send(ranking.rows);
-	} catch (error) {}
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
 }
 
 export { getUserHistoric, usersRanking };
