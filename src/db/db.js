4f87fabff5a7e1d74b632b47d39580152cc939df
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-17 12:21:53 -0300

    feat: Preparação do servidor para deploy no heroku.

--- src/db/db.js
+++ src/db/db.js
@@ -3,11 +3,8 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-	user: 'postgres',
-	password: '123456',
-	host: 'localhost',
-	port: 5432,
-	database: 'shortly',
+	connectionString: process.env.DATABASE_URL,
+	ssl: { rejectUnauthorized: false },
 });
 
 export { connection };
