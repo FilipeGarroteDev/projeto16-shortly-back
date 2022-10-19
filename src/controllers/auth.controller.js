3618a172edf287d552b70282b5d477d5f71ec1b6
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-19 14:58:58 -0300

    fix: ajustes heroku

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ async function createUser(req, res) {
 
 		const teste = authRepository.createUser(name, email, encryptedPassword);
 
-		res.status(201).send(teste.rows);
+		res.status(201).send(teste);
 	} catch (error) {
 		return res.status(500).send(error.message);
 	}
